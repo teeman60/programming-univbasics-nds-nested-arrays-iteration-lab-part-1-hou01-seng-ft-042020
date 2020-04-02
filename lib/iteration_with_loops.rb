f16e899f6a159cd08d2fbf60bb64d2b34ce2c8fe
Taiye Salami <taiye41@gmail.com>
2020-04-02 11:17:51 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,9 +6,9 @@ def find_even_values(src)
   while row < src.length do
     column = 0
     while column < src[row].length do
-      if src[row][column] % 2 == 0
-      puts src[row][column]
-      end
+      puts src[row][column] % 2 == 0
+      # puts src[row][column]
+      # end
       column += 1
     end
     row += 1
